0e678772c5e02a447990ae0edb40a1509bfcecd7
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@gmail.com>
2020-07-03 23:24:16 -0300

    retirando o comentário das atividades anteriores do módulo.

--- Modulo6/Program.cs
+++ Modulo6/Program.cs
@@ -9,51 +9,51 @@ namespace Modulo6
     {
         static void Main(string[] args)
         {
-            //int QuantidadeDeQuartos = 10;
+            int QuantidadeDeQuartos = 10;
 
-            //Console.WriteLine("--== Pensionato Da Dona ==--");
+            Console.WriteLine("--== Pensionato Da Dona ==--");
 
-            //Console.Write("Quantos quartos serão alugados? -> ");
-            //int NumeroDeQuartos = int.Parse(Console.ReadLine());
+            Console.Write("Quantos quartos serão alugados? -> ");
+            int NumeroDeQuartos = int.Parse(Console.ReadLine());
 
-            //Hospede[] ListaDeQuartos = new Hospede[QuantidadeDeQuartos];
+            Hospede[] ListaDeQuartos = new Hospede[QuantidadeDeQuartos];
 
-            //for (int i = 0; i < NumeroDeQuartos; i++)
-            //{
-            //    Console.Write("Digite o nome do hóspede: ");
-            //    string Nome = Console.ReadLine();
+            for (int i = 0; i < NumeroDeQuartos; i++)
+            {
+                Console.Write("Digite o nome do hóspede: ");
+                string Nome = Console.ReadLine();
 
-            //    Console.Write("Digite o email do hóspede: ");
-            //    string Email = Console.ReadLine();
+                Console.Write("Digite o email do hóspede: ");
+                string Email = Console.ReadLine();
 
-            //    Console.Write("Digite o número do quarto desejado: ");
-            //    int Quarto = int.Parse(Console.ReadLine());
+                Console.Write("Digite o número do quarto desejado: ");
+                int Quarto = int.Parse(Console.ReadLine());
 
-            //    Console.WriteLine(" ");
+                Console.WriteLine(" ");
 
-            //    ListaDeQuartos[Quarto] = new Hospede(Nome, Email);
-            //}
+                ListaDeQuartos[Quarto] = new Hospede(Nome, Email);
+            }
 
-            //Console.WriteLine("Quartos Ocupados: ");
+            Console.WriteLine("Quartos Ocupados: ");
 
-            //for (int i = 0; i < QuantidadeDeQuartos; i++)
-            //{
-            //    if (ListaDeQuartos[i] != null)
-            //    {
-            //        Console.WriteLine(i + ": " + ListaDeQuartos[i].Nome + ", " + ListaDeQuartos[i].Email);
-            //    }
-            //}
+            for (int i = 0; i < QuantidadeDeQuartos; i++)
+            {
+                if (ListaDeQuartos[i] != null)
+                {
+                    Console.WriteLine(i + ": " + ListaDeQuartos[i].Nome + ", " + ListaDeQuartos[i].Email);
+                }
+            }
 
-            //Console.WriteLine(Calculator.Sum(10, 20, 30, 40));
+            Console.WriteLine(Calculator.Sum(10, 20, 30, 40));
 
-            //int N = 10;
-            //Calculator.Triplicar(ref N);
-            //Console.WriteLine(N);
+            int N = 10;
+            Calculator.Triplicar(ref N);
+            Console.WriteLine(N);
 
-            //int N2 = 10;
-            //int Dobrado;
-            //Calculator.Dobrar(N2, out Dobrado);
-            //Console.WriteLine($"O dobro de: {N2}, é = {Dobrado}");
+            int N2 = 10;
+            int Dobrado;
+            Calculator.Dobrar(N2, out Dobrado);
+            Console.WriteLine($"O dobro de: {N2}, é = {Dobrado}");
 
             Console.Write("Quantos Funcionários Deseja Cadastrar? ");
             int NumeroRegistros = int.Parse(Console.ReadLine());
