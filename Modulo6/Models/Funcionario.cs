bd962ab48c05de2c4cb850f41e2174b6f183b3c3
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@gmail.com>
2020-07-03 23:22:05 -0300

    Atividade da aula sobre Listas (List)

---
+++ Modulo6/Models/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Modulo6.Models
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public double Salario { get; private set; }
+
+        private static double CEM = 100;
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public void AcrescimoDeSalario(double porcentagem)
+        {
+            Salario = ((porcentagem / CEM) * Salario) + Salario;
+        }
+    }
+}
--- Modulo6/Program.cs
+++ Modulo6/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Modulo6.Models;
 using System;
+using System.Collections.Generic;
+using System.Dynamic;
 
 namespace Modulo6
 {
@@ -7,51 +9,98 @@ namespace Modulo6
     {
         static void Main(string[] args)
         {
-            int QuantidadeDeQuartos = 10;
+            //int QuantidadeDeQuartos = 10;
 
-            Console.WriteLine("--== Pensionato Da Dona ==--");
+            //Console.WriteLine("--== Pensionato Da Dona ==--");
 
-            Console.Write("Quantos quartos serão alugados? -> ");
-            int NumeroDeQuartos = int.Parse(Console.ReadLine());
+            //Console.Write("Quantos quartos serão alugados? -> ");
+            //int NumeroDeQuartos = int.Parse(Console.ReadLine());
 
-            Hospede[] ListaDeQuartos = new Hospede[QuantidadeDeQuartos];
+            //Hospede[] ListaDeQuartos = new Hospede[QuantidadeDeQuartos];
 
-            for (int i = 0; i < NumeroDeQuartos; i++)
+            //for (int i = 0; i < NumeroDeQuartos; i++)
+            //{
+            //    Console.Write("Digite o nome do hóspede: ");
+            //    string Nome = Console.ReadLine();
+
+            //    Console.Write("Digite o email do hóspede: ");
+            //    string Email = Console.ReadLine();
+
+            //    Console.Write("Digite o número do quarto desejado: ");
+            //    int Quarto = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine(" ");
+
+            //    ListaDeQuartos[Quarto] = new Hospede(Nome, Email);
+            //}
+
+            //Console.WriteLine("Quartos Ocupados: ");
+
+            //for (int i = 0; i < QuantidadeDeQuartos; i++)
+            //{
+            //    if (ListaDeQuartos[i] != null)
+            //    {
+            //        Console.WriteLine(i + ": " + ListaDeQuartos[i].Nome + ", " + ListaDeQuartos[i].Email);
+            //    }
+            //}
+
+            //Console.WriteLine(Calculator.Sum(10, 20, 30, 40));
+
+            //int N = 10;
+            //Calculator.Triplicar(ref N);
+            //Console.WriteLine(N);
+
+            //int N2 = 10;
+            //int Dobrado;
+            //Calculator.Dobrar(N2, out Dobrado);
+            //Console.WriteLine($"O dobro de: {N2}, é = {Dobrado}");
+
+            Console.Write("Quantos Funcionários Deseja Cadastrar? ");
+            int NumeroRegistros = int.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            List<Funcionario> ListaFuncionario = new List<Funcionario>();
+
+            for (int i = 0; i < NumeroRegistros; i++)
             {
-                Console.Write("Digite o nome do hóspede: ");
+                Console.WriteLine($"Cadastro do funcionário {i+1}");
+                
+                Console.Write("ID: ");
+                int ID = int.Parse(Console.ReadLine());
+
+                Console.Write("Nome: ");
                 string Nome = Console.ReadLine();
 
-                Console.Write("Digite o email do hóspede: ");
-                string Email = Console.ReadLine();
+                Console.Write("Salário: ");
+                double Salario = double.Parse(Console.ReadLine());
+                Console.WriteLine(" ");                             
 
-                Console.Write("Digite o número do quarto desejado: ");
-                int Quarto = int.Parse(Console.ReadLine());
+                ListaFuncionario.Add(new Funcionario(ID, Nome, Salario));
+            }
 
+            Console.Write("Digite o ID de qual funcionário vai receber a porcentagem: ");
+            int IdAumento = int.Parse(Console.ReadLine());
+
+            Funcionario Func = ListaFuncionario.Find(x => x.Id == IdAumento);
+            if (Func != null)
+            {
+                Console.Write("Digite a porcentagem a ser aumentada: ");
+                double Acrescimo = double.Parse(Console.ReadLine());
                 Console.WriteLine(" ");
 
-                ListaDeQuartos[Quarto] = new Hospede(Nome, Email);
+                Func.AcrescimoDeSalario(Acrescimo);
             }
-
-            Console.WriteLine("Quartos Ocupados: ");
-            
-            for (int i = 0; i < QuantidadeDeQuartos; i++)
+            else
             {
-                if (ListaDeQuartos[i] != null)
-                {
-                    Console.WriteLine(i + ": " + ListaDeQuartos[i].Nome + ", " + ListaDeQuartos[i].Email);
-                }
+                Console.WriteLine("O ID informado não existe no sistema!");
+                Console.WriteLine(" ");
             }
 
-            Console.WriteLine(Calculator.Sum(10, 20, 30, 40));
-
-            int N = 10;
-            Calculator.Triplicar(ref N);
-            Console.WriteLine(N);
-
-            int N2 = 10;
-            int Dobrado;
-            Calculator.Dobrar(N2, out Dobrado);
-            Console.WriteLine($"O dobro de: {N2}, é = {Dobrado}");
+            Console.WriteLine("--==Lista de Funcionários==--");
+            foreach (var funcinario in ListaFuncionario)
+            {
+                Console.WriteLine(funcinario.ToString());
+            }
 
         }
     }
